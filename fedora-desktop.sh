c1cb0c9b50f26b7fcbe3b13586408804fbd3c4f9
pedro <pedrohenriquecarneiro143@gmail.com>
2021-11-21 16:23:28 -0300

    adding intellij and fixing some typos

--- fedora-desktop.sh
+++ fedora-desktop.sh
@@ -21,6 +21,18 @@ sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg  &&
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo &&
 
+#Git config
+git config --global user.email "pedrohenriquecarneiro143@gmail.com" &&
+git config --global user.email "pedro" &&
+
+#Downloading and Installing Intellij idea
+cd $HOME/Downloads &&
+wget -c https://www.jetbrains.com/pt-br/idea/download/download-thanks.html?platform=linux&code=IIC  &&
+tar xvzf ~/Downloads/idea*.tar.gz -C /tmp/ &&
+sudo chown -R root:root /tmp/idea* &&
+sudo mv /tmp/idea* /opt/idea &&
+sudo ln -s /opt/idea/bin/idea.sh /usr/local/bin/idea.sh &&
+
 #Brave web-browser enable repo
 sudo dnf install dnf-plugins-core &&
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/ &&
@@ -29,7 +41,7 @@ sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc 
 #Install the programs
 
 #These are for themes and extensions
-sudo dnf install gnome-tweak-tool gnome-extensions -y &&
+sudo dnf install gnome-tweaks gnome-extensions -y &&
 
 #Desing and music
 sudo dnf install gimp inkscape blender ardour6 -y &&
@@ -42,6 +54,8 @@ sudo dnf install java java-devel make automake gcc gcc-c++ kernel-devel -y &&
 
 #Nvidia drivers
 sudo dnf install akmod-nvidia -y &&
+sudo dnf install xorg-x11-drv-nvidia-cuda -y &&
 
 #Daily use
 sudo dnf install qbittorrent thunderbird brave-browser -y
+
