70c58452b306f536c93c4fabfd048528a8e461d2
Pedro <86194799+pedrocarne@users.noreply.github.com>
2022-04-19 20:42:09 -0300

    Updating email and git user

--- fedora-desktop.sh
+++ fedora-desktop.sh
@@ -22,8 +22,8 @@ sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg" |sudo tee -a /etc/yum.repos.d/vscodium.repo &&
 
 #Git config
-git config --global user.email "pedrohenriquecarneiro143@gmail.com" &&
-git config --global user.email "pedro" &&
+git config --global user.email "86194799+pedrocarne@users.noreply.github.com" &&
+git config --global user.email "Pedro" &&
 
 #Downloading and Installing Intellij idea
 cd $HOME/Downloads &&
